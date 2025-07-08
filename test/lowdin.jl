cb6a1df654f0ef5df61e7cfb9ea6483fdd309ee1
Shrikhande Chinmay <chinmays@vt.edu>
2025-07-08 17:07:02 -0400

    modified:   test/lowdin.jl

--- test/lowdin.jl
+++ test/lowdin.jl
@@ -6,6 +6,7 @@ using Plots, Measures
 using Printf
 using OrderedCollections
 using StatProfilerHTML
+using BenchmarkTools
 gr(display_type=:inline)
 
 
@@ -67,7 +68,7 @@ end
 
 
 function matrix_run()
-    N = 6
+    N = 4
     dim = 2^N
 
     # Initializing the Lindbladian
@@ -75,8 +76,8 @@ function matrix_run()
     add_hamiltonian!(L, OpenSCI.heisenberg_1D(N, 1.1, 1.2, 1.3))
     add_channel_dephasing!(L, 0.1)  
     add_channel_depolarizing!(L, .1)
-    println("Lindbladian: ")
-    display(L)
+    # println("Lindbladian: ")
+    # display(L)
 
     Lmat = Matrix(L)
     dim_L = size(Lmat, 1)
@@ -94,14 +95,14 @@ function matrix_run()
         @printf(" %4i %12.8f %12.8fi Tr = %12.8f\n", i, real(F.values[i]), imag(F.values[i]), real(tr(states[i])))
     end
 
-    display(size(Lmat))
+    display(size(Lmat))   
 
     state = DyadSum(Dyad(N, 0, 0))
     # vec_state_i = vec(Matrix(state))
     nkeep = 2
     v0 = SparseDyadVectors(state, R = nkeep)
 
-    p_dyad, eig_sci = selected_ci(L, v0, max_iter_outer = 5)
+    p_dyad, eig_sci = selected_ci(L, v0, max_iter_outer = 3)
     println("\n Eigenvalues after SCI")
     display(eig_sci)
 
@@ -134,14 +135,16 @@ function matrix_run()
     n = size(lmat_xx)[1]
     identity_matrix = Matrix{Float64}(I, n, n)
 
-    corr_idx = 2
+    corr_idx = 1
     lambda = eig_sci[corr_idx]*identity_matrix
 
     pertubation  = lmat_px * inv(lambda - lmat_xx) * lmat_xp
     leff = lmat_pp + pertubation
-    # println("Pertubation")
-    # display(pertubation)
+    # println("\n Temp")
+    # display(lmat_xx)
 
+    # println("X-space")
+    # display(x_dyad)
     e, v = eigen(leff)
     println("\n Eigenvalues after Lowdin")
     new_eig = e[end-nkeep+1:end]
@@ -159,41 +162,23 @@ function matrix_run()
 end
 
 function left_eigenvectors(dyad_dict::SparseDyadVectors{N,T}, Lmat)::SparseDyadVectors{N,T} where {N,T}
-    # dyad_keys = collect(keys(dyad_dict))                            
-    # values_matrix = transpose(hcat(values(dyad_dict)...))           
+          
     e = 0
     v = zeros(T,size(dyad_dict))
-    display(size(dyad_dict))
     Lmat = Lmat'
     dim, R = size(dyad_dict)
     if length(dyad_dict) < 300
         e,v = eigen(Lmat)
         e = e[end-R+1:end]
         v = v[:, end-R+1:end]
-        
-        println("Eigen for left")
-        display(e)
     else
         e,v = eigs(Lmat, nev=R, v0=Matrix(Pv)[:,1], which=:LR, maxiter=3000 , tol=1e-5, check=1)
-        println("Eigs")
         perm = sortperm(real(e))
         e = e[perm]
         v = v[:, perm]
     end
     
-    # perm = sortperm(real(e))
-    # e = e[perm[end-R+1:end]]
-    # v = v[:, perm[end-R+1:end]]
-    
     OpenSCI.fill!(dyad_dict, v)
-    # pinv_matrix = pinv(values_matrix)                              
-    # dim, R = size(dyad_dict)
-    # pinv_sdv = OrderedDict{DyadBasis{N}, Vector{T}}()
-    # pinv_sdv = SparseDyadVectors(DyadSum(N), R = R)
-    # for i in eachindex(dyad_keys)
-    #     pinv_sdv[dyad_keys[i]] = vec(pinv_matrix[:, i])           
-    # end
-
     return dyad_dict
 end
 
@@ -207,8 +192,8 @@ function dyad_run()
     add_hamiltonian!(L, OpenSCI.heisenberg_1D(N, 1.1, 1.2, 1.3))
     add_channel_dephasing!(L, 0.1)  
     add_channel_depolarizing!(L, .1)
-    println("Lindbladian: ")
-    display(L)
+    # println("Lindbladian: ")
+    # display(L)
 
     Lmat = Matrix(L)
     dim_L = size(Lmat, 1)
@@ -232,22 +217,13 @@ function dyad_run()
     nkeep = 2
     v0 = SparseDyadVectors(state, R = nkeep)
 
-    p_dyad, eig_sci = selected_ci(L, v0, max_iter_outer = 1)
+    p_dyad, eig_sci = selected_ci(L, v0, max_iter_outer = 4)
     println("\n Eigenvalues after SCI")
     display(eig_sci)
-    # lmat_pp = build_subspace_L(L, p_dyad)
-    # println("P-Space")
-    # display(lmat_pp)
+
+
+    # this is for Q * L * P |vi>>
     x_temp = L * p_dyad
-    # println("P-Space")
-    # display(p_dyad)
-
-    p_projector = SparseDyadVectors(DyadSum(N), R = 1)
-    proj_coeff = fill(1, 1)
-
-    for (d, c) in p_dyad
-        p_projector[d] = proj_coeff
-    end
 
     x_dyad = SparseDyadVectors(DyadSum(N), R=nkeep)
 
@@ -257,41 +233,166 @@ function dyad_run()
         end
     end 
 
-    x_projector =SparseDyadVectors(DyadSum(N), R = 1)
-    for (d, coeff) in x_temp
-        x_projector[d] = proj_coeff
-    end  
+    # different approach
 
-    # display(length(x_dyad))
-    # display(p_dyad' * (L*p_dyad))
+    # chi_coeffs = SparseDyadVectors(DyadSum(N), R=nkeep)
 
-    # now getting the correction for the eigenvalues 
+    # for (q_dyad, _) in x_dyad  
+
+    #     γ = zeros(ComplexF64, 1, nkeep)
+    #     γ = vec(γ)
+
+    #     for (p_k, d_k) in p_dyad
+
+    #         p_vec = SparseDyadVectors(DyadSum(p_k), R = 1)
+    #         L_p = L * p_vec
+    #         coeff = get(L_p, q_dyad, 0.0 + 0im)  
+    #         γ .+= d_k .* coeff               
+    #     end
+    #     chi_coeffs[q_dyad] = γ
+    # end
+
+    
+    # here we get the <<w_i| P * L * Q
+    left_p_dyad = left_eigenvectors(p_dyad, build_subspace_L(L, p_dyad))
+
+    gamma_coeffs = SparseDyadVectors(DyadSum(N), R=nkeep)
+
+    for (q_dyad, _) in x_dyad  
+        q_vec = SparseDyadVectors(DyadSum(q_dyad), R = 1)
+
+        L_q = L * q_vec
+
+        γ = zeros(ComplexF64, 1, nkeep)
+        γ = vec(γ)
+        for (p_k, d_k) in left_p_dyad
+            coeff = get(L_q, p_k, 0.0 + 0im)  
+            γ .+= conj.(d_k) .* coeff               
+        end
+        gamma_coeffs[q_dyad] = γ
+    end
+       
+    xx_coeffs = SparseDyadVectors(DyadSum(N), R=nkeep)
+
+    for (q_dyad, _) in x_dyad  
+        q_vec = SparseDyadVectors(DyadSum(q_dyad), R = nkeep)
+
+        L_q = L * q_vec
+
+        coeff = get(L_q, q_dyad, 0.0 + 0im)         
+        xx_coeffs[q_dyad] = coeff
+    end
+
+    correction = 0
     c_idx = 1
 
-    l_x_dyad = L * x_projector
+    for (q_dyad, coeff) in x_dyad
+        correction += gamma_coeffs[q_dyad][c_idx] * coeff[c_idx] / (eig_sci[c_idx] - xx_coeffs[q_dyad][c_idx])
+    end
 
-    l_px = p_projector' * l_x_dyad 
+    println("Correction")
 
-    l_xp = x_projector' * (L * p_projector)
-    l_xx = x_projector' * l_x_dyad
-
-    # correction = l_px[c_idx, c_idx] * inv(eig_sci[c_idx] - l_xx[c_idx, c_idx]) * l_xp[c_idx, c_idx]
-    correction = l_px[1, 1] * inv(eig_sci[c_idx] - l_xx[1, 1]) * l_xp[1, 1]
-
-    println("\nTemp")
-
-    # display(l_px)
-    # a = left_eigenvectors(p_dyad, build_subspace_L(L, p_dyad))
-    # display(inv(eig_sci[c_idx] - l_xx[1, 1]))
-
-    println("\n Correction")
     display(correction)
     corr_eig = eig_sci[c_idx] + correction
 
     println("\n Corrected Eigenvalue")
     display(corr_eig)
+
+    return
+
+end
+
+using LinearAlgebra
+using Printf
+# Assuming OpenSCI, SparseDyadVectors, etc. are defined elsewhere
+# include("path/to/OpenSCI_definitions.jl")
+
+function dyad_run_optimized()
+    N = 4
+    dim = 2^N
+
+    # Setup Lindbladian 
+    L = Lindbladian(N)
+    add_hamiltonian!(L, OpenSCI.heisenberg_1D(N, 1.1, 1.2, 1.3))
+    add_channel_dephasing!(L, 0.1)
+    add_channel_depolarizing!(L, 0.1)
+
+    Lmat = Matrix(L)
+    dim_L = size(Lmat, 1)
+    
+    # Exact Diagonalization
+    F = eigen(Lmat)
+    perm = sortperm(F.values, by=real)
+    F.values .= F.values[perm]
+    F.vectors .= F.vectors[:, perm]
+    states = [reshape(F.vectors[:,i], 2^N, 2^N)/sqrt(2^N) for i in 1:length(F.values)]
+    @printf(" Eigenvalues of L:\n")
+    for i in (length(F.values) - 2):length(F.values)
+        @printf(" %4i %12.8f %12.8fi Tr = %12.8f\n", i, real(F.values[i]), imag(F.values[i]), real(tr(states[i])))
+    end
+    display(size(Lmat))
+
+    # Selected CI
+    state = DyadSum(Dyad(N, 0, 0))
+    nkeep = 2
+    v0 = SparseDyadVectors(state, R=nkeep)
+    p_dyad, eig_sci = selected_ci(L, v0, max_iter_outer=3)
+    println("\n Eigenvalues after SCI")
+    display(eig_sci)
+
+    # Get the component of L*|V>> in the external space Q
+    # This represents <<q|L|v_i>>
+    x_temp = L * p_dyad
+    x_dyad = SparseDyadVectors(DyadSum(N), R=nkeep)
+    for (d, coeff) in x_temp
+        if !haskey(p_dyad, d)
+            sum!(x_dyad, d, coeff)
+        end
+    end
+
+    # Get the left eigenvectors <<W_i| of the projected Lindbladian P*L*P
+    left_p_dyad = left_eigenvectors(p_dyad, build_subspace_L(L, p_dyad))
+
+    correction_vec = zeros(ComplexF64, nkeep)
+    
+    for (q_dyad, coeff) in x_dyad
+        
+        # Applying L to |q>> and projecting onto the left eigenvectors <<W_i|.
+        q_vec_scalar = SparseDyadVectors(DyadSum(q_dyad), R = 1)
+        L_q_scalar = L * q_vec_scalar 
+        
+        num_1 = zeros(ComplexF64, nkeep)
+        num_1 = vec(num_1)
+        for (p_k, d_k_vec) in left_p_dyad
+            Lq_pk_coeff = get(L_q_scalar, p_k, 0.0 + 0im)
+            num_1 .+= conj.(d_k_vec) .* Lq_pk_coeff
+        end
+
+        # Calculate the denominator: E_i - <<q|L|q>>
+        q_vec_vector = SparseDyadVectors(DyadSum(q_dyad), R = nkeep)
+        L_q_vector = L * q_vec_vector 
+        
+        denominator_diag_vec = get(L_q_vector, q_dyad, zeros(ComplexF64, nkeep))
+        energy_diff_vec = eig_sci .- denominator_diag_vec
+
+        term_vec = (num_1 .* coeff) ./ energy_diff_vec
+        
+        correction_vec .+= term_vec
+    end
+
+    c_idx = 1
+    correction = correction_vec[c_idx]
+    
+    println("Correction")
+    display(correction)
+    
+    corr_eig = eig_sci[c_idx] + correction
+    println("\n Corrected Eigenvalue")
+    display(corr_eig)
+
     return
 end
 
-dyad_run()
-# matrix_run()
+# @btime dyad_run()
+@time matrix_run()
+# @time dyad_run_optimized()
